d8f8854717c39c35c96b41cc53a7a3e06c21a9bc
gabriel augusto <gabriel.augusto2504@gmail.com>
2024-04-17 16:26:18 -0300

    create person

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_17_191527) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_17_192556) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,4 +20,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_17_191527) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "people", force: :cascade do |t|
+    t.string "name"
+    t.string "phone_number"
+    t.string "cpf"
+    t.bigint "company_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["company_id"], name: "index_people_on_company_id"
+  end
+
+  add_foreign_key "people", "companies"
 end

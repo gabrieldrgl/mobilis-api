544ffb6660e038ef69c518b69ffbd3ba38c110f5
Gabriel Augusto <gabriel.augusto2504@gmail.com>
2024-09-07 14:19:28 -0300

    create company

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_07_135422) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_07_171903) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "devise_api_tokens", force: :cascade do |t|
     t.string "resource_owner_type", null: false
